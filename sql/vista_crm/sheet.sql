1c964600407a1860fe2dadb3e3776d8304c5c8bd
jhamerski <jonashamerski87@gmail.com>
2025-06-05 17:58:16 -0300

    VISTA-CRM: New sheet

--- sql/vista_crm/sheet.sql
+++ sql/vista_crm/sheet.sql
@@ -1,39 +1,3 @@
-# PIPELINES
-CREATE OR REPLACE EXTERNAL TABLE {project_id}.vista_crm.pipelines
-OPTIONS (
-  format = 'CSV',
-  field_delimiter=';',
-  skip_leading_rows=1,
-  allow_quoted_newlines=true,
-  uris = ['gs://{bucket_name}/pipelines/pipelines.csv']);
-
-# ETAPAS
-CREATE OR REPLACE EXTERNAL TABLE {project_id}.vista_crm.etapas
-OPTIONS (
-  format = 'CSV',
-  field_delimiter=';',
-  skip_leading_rows=1,
-  allow_quoted_newlines=true,
-  uris = ['gs://{bucket_name}/etapas/etapas.csv']);
-
-# NEGOCIOS
-CREATE OR REPLACE EXTERNAL TABLE {project_id}.vista_crm.negocios
-OPTIONS (
-  format = 'CSV',
-  field_delimiter=';',
-  skip_leading_rows=1,
-  allow_quoted_newlines=true,
-  uris = ['gs://{bucket_name}/negocios/negocios.csv']);
-
-# CAMPOS_CLIENTES
-CREATE OR REPLACE EXTERNAL TABLE {project_id}.vista_crm.campos_clientes
-OPTIONS (
-  format = 'CSV',
-  field_delimiter=';',
-  skip_leading_rows=1,
-  allow_quoted_newlines=true,
-  uris = ['gs://{bucket_name}/campos_clientes/campos_clientes.csv']);
-
 # CLIENTES
 CREATE OR REPLACE EXTERNAL TABLE {project_id}.vista_crm.clientes
 OPTIONS (
@@ -41,16 +5,34 @@ OPTIONS (
   field_delimiter=';',
   skip_leading_rows=1,
   allow_quoted_newlines=true,
-  uris = ['gs://{bucket_name}/clientes/clientes.csv']);
+  uris = ['gs://{bucket_name}/tabela_clientes/clientes.csv']);
 
-# CAMPOS_IMOVEIS
-CREATE OR REPLACE EXTERNAL TABLE {project_id}.vista_crm.campos_imoveis
+# CORRETORES
+CREATE OR REPLACE EXTERNAL TABLE {project_id}.vista_crm.corretores
 OPTIONS (
   format = 'CSV',
   field_delimiter=';',
   skip_leading_rows=1,
   allow_quoted_newlines=true,
-  uris = ['gs://{bucket_name}/campos_imoveis/campos_imoveis.csv']);
+  uris = ['gs://{bucket_name}/tabela_corretores/corretores.csv']);
+
+# FUNIL_LOCACAO
+CREATE OR REPLACE EXTERNAL TABLE {project_id}.vista_crm.funil_locacao
+OPTIONS (
+  format = 'CSV',
+  field_delimiter=';',
+  skip_leading_rows=1,
+  allow_quoted_newlines=true,
+  uris = ['gs://{bucket_name}/funil_locacao/funil_locacao.csv']);
+
+# FUNIL_VENDAS
+CREATE OR REPLACE EXTERNAL TABLE {project_id}.vista_crm.funil_vendas
+OPTIONS (
+  format = 'CSV',
+  field_delimiter=';',
+  skip_leading_rows=1,
+  allow_quoted_newlines=true,
+  uris = ['gs://{bucket_name}/funil_vendas/funil_vendas.csv']);
 
 # IMOVEIS
 CREATE OR REPLACE EXTERNAL TABLE {project_id}.vista_crm.imoveis
@@ -59,44 +41,32 @@ OPTIONS (
   field_delimiter=';',
   skip_leading_rows=1,
   allow_quoted_newlines=true,
-  uris = ['gs://{bucket_name}/imoveis/imoveis.csv']);
+  uris = ['gs://{bucket_name}/tabela_imoveis/imoveis.csv']);
 
 -- GOLD
-# PIPELINES
-CREATE OR REPLACE TABLE `{project_id}.vendas.vista_crm_pipelines_gold`
-AS
-SELECT *
-FROM `{project_id}.vista_crm.pipelines`;
-
-# ETAPAS
-CREATE OR REPLACE TABLE `{project_id}.vendas.vista_crm_etapas_gold`
-AS
-SELECT *
-FROM `{project_id}.vista_crm.etapas`;
-
-# NEGOCIOS
-CREATE OR REPLACE TABLE `{project_id}.vendas.vista_crm_negocios_gold`
-AS
-SELECT *
-FROM `{project_id}.vista_crm.negocios`;
-
-# CAMPOS_CLIENTES
-CREATE OR REPLACE TABLE `{project_id}.vendas.vista_crm_campos_clientes_gold`
-AS
-SELECT *
-FROM `{project_id}.vista_crm.campos_clientes`;
-
 # CLIENTES
 CREATE OR REPLACE TABLE `{project_id}.vendas.vista_crm_clientes_gold`
 AS
 SELECT *
 FROM `{project_id}.vista_crm.clientes`;
 
-# CAMPOS_IMOVEIS
-CREATE OR REPLACE TABLE `{project_id}.vendas.vista_crm_campos_imoveis_gold`
+# CORRETORES
+CREATE OR REPLACE TABLE `{project_id}.vendas.vista_crm_corretores_gold`
 AS
 SELECT *
-FROM `{project_id}.vista_crm.campos_imoveis`;
+FROM `{project_id}.vista_crm.corretores`;
+
+# FUNIL_LOCACAO
+CREATE OR REPLACE TABLE `{project_id}.vendas.vista_crm_funil_locacao_gold`
+AS
+SELECT *
+FROM `{project_id}.vista_crm.funil_locacao`;
+
+# FUNIL_VENDAS
+CREATE OR REPLACE TABLE `{project_id}.vendas.vista_crm_funil_vendas_gold`
+AS
+SELECT *
+FROM `{project_id}.vista_crm.funil_vendas`;
 
 # IMOVEIS
 CREATE OR REPLACE TABLE `{project_id}.vendas.vista_crm_imoveis_gold`
