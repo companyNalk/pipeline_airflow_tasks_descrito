3569a72db4d11b3481980514ea8026f09e75fd2c
jhamerski <jonashamerski87@gmail.com>
2025-06-05 16:45:09 -0300

    VISTA-CRM: All endpoints

--- legacy-integrations/vista_crm.py
+++ legacy-integrations/vista_crm.py
@@ -2370,24 +2370,24 @@ def get_extraction_tasks():
         list: List of task configurations
     """
     return [
-        # {
-        #     'task_id': 'extract_customers',
-        #     'python_callable': extract_customers
-        # },
+        {
+            'task_id': 'extract_customers',
+            'python_callable': extract_customers
+        },
         {
             'task_id': 'extract_realtor',
             'python_callable': extract_realtor
         },
-        # {
-        #     'task_id': 'extract_rental_funnel',
-        #     'python_callable': extract_rental_funnel
-        # },
-        # {
-        #     'task_id': 'extract_sales_funnel',
-        #     'python_callable': extract_sales_funnel
-        # },
-        # {
-        #     'task_id': 'extract_real_state',
-        #     'python_callable': extract_real_state
-        # }
+        {
+            'task_id': 'extract_rental_funnel',
+            'python_callable': extract_rental_funnel
+        },
+        {
+            'task_id': 'extract_sales_funnel',
+            'python_callable': extract_sales_funnel
+        },
+        {
+            'task_id': 'extract_real_state',
+            'python_callable': extract_real_state
+        }
     ]
